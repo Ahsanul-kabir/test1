11c1d7754d907c56e752a42c7ae9089194c1e844
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-03-20 20:24:49 +0600

    bruj-al-arab-done

---
+++ src/components/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDtLmwdsZ1XjChoY6woGOqopd9OKFo9Pr4",
+  authDomain: "bruj-al-arab-7.firebaseapp.com",
+  projectId: "bruj-al-arab-7",
+  storageBucket: "bruj-al-arab-7.appspot.com",
+  messagingSenderId: "1051609759865",
+  appId: "1:1051609759865:web:b688a7fb3dbff92dbcd4a8"
+};
+export default firebaseConfig;
